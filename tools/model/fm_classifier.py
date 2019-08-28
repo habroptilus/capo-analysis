b8c98ed777ef2178708f4a828ac6726d348f3c72
habroptilus-smn-mac <hikaru_ogura@so-netmedia.jp>
2019-08-28 17:41:13 +0900

    fmcls motoolへ

---
+++ tools/model/fm_classifier.py
@@ -0,0 +1,62 @@
+from sklearn.metrics import accuracy_score
+from fastFM import als
+import numpy as np
+
+
+class FMClassifier:
+    def __init__(self, n_iter, rank, preprocessor):
+        self.model = als.FMClassification(n_iter=n_iter, rank=rank)
+        self.preprocessor = preprocessor
+
+    def fit(self, X, y):
+        X, y = self.preprocessor.preprocess(X, y, do_sampling=True)
+        return self.model.fit(X, y)
+
+    def predict(self, X):
+        """
+        :param X: np.arrayでchord featureのみ。複製前.
+        :param y: recomennded capo. 文字列. len(X)=len(y)
+         :return e.g. np.array([["+1", "-4", "0",...],["-1", "-3", "+1",...],...])
+        """
+        rankings = self.predict_ranking(X)
+        return np.array([ranking[0] for ranking in rankings])
+
+    def predict_ranking(self, X):
+        """入力した曲に対しておすすめcapoのランキングを返す.
+
+         :param X: np.arrayでchord featureのみ。複製前.
+         :return e.g. np.array(["+1", "-4", "0",...])
+        """
+        X = self.preprocessor.preprocess(X, do_sampling=False)
+        pred = self.model.predict(X)
+        reshaped = np.reshape(pred, (-1, len(self.preprocessor.dupulication_capo_order)))
+        results = []
+        for i in range(len(reshaped)):
+            results.append(self.return_sorted_capo(reshaped[i]))
+        return results
+
+    def get_all_embeddings(self):
+        """embeddingを全てまとめて取得"""
+        return self.model.V_.T
+
+    def get_embeddings(self):
+        """学習結果としてchordとcapoのembeddingを取得"""
+        embeddings = self.get_all_embeddings()
+        chord_embeddings = embeddings[:-len(self.preprocessor.dupulication_capo_order), :]
+        capo_embeddings = embeddings[-len(self.preprocessor.dupulication_capo_order):, :]
+        return chord_embeddings, capo_embeddings
+
+    def evaluate_top1(self, X, y):
+        """
+        :param X: np.arrayでchord featureのみ。複製前.
+        :param y: recomennded capo. 文字列. len(X)=len(y)
+        """
+        pred = self.predict(X)
+        return accuracy_score(y, pred)
+
+    def return_sorted_capo(self, pred):
+        """予測したscoreを元にrankingになるようにsortする."""
+        index = [0] * len(self.preprocessor.dupulication_capo_order)
+        for j, rank in enumerate(np.argsort(pred[::-1])):
+            index[rank] = j
+        return self.preprocessor.dupulication_capo_order[index]
---
+++ tools/preprocess/cls_preprocessor.py
@@ -0,0 +1,76 @@
+from scipy import sparse
+import numpy as np
+from sklearn.preprocessing import LabelBinarizer
+from numpy.random import randint
+
+
+class CLSPreprocessor:
+    """FMClassifier用のpreprocessor."""
+
+    def __init__(self, dupulication_capo_order):
+        self.dupulication_capo_order = dupulication_capo_order
+        self.capo_encoder = None
+
+    def preprocess(self, X, y=None, do_sampling=False):
+        """preprocess本体.fitの時はdo_sampling=Trueを想定."""
+        X = self.duplicate_add_capo_features(X)
+        if y is None:
+            return sparse.csr_matrix(X)
+        y = self.create_labels(y)
+        if do_sampling:
+            X, y = self.sample_dataset(X, y)
+        X = sparse.csr_matrix(X)
+        return X, y
+
+    def shuffle_samples(self, X, y):
+        """samplingした後にshuffleする."""
+        zipped = list(zip(X, y))
+        np.random.shuffle(zipped)
+        X_result, y_result = zip(*zipped)
+        return np.asarray(X_result), np.asarray(y_result)
+
+    def sample_dataset(self, X_dup, labels):
+        """複製したX, yからsamplingする.正例と負例は1:1に"""
+        X_dup_pos = X_dup[labels == 1]
+        X_dup_neg = X_dup[labels == -1]
+        diff = randint(0, len(self.dupulication_capo_order) - 1, len(X_dup_neg) // (len(self.dupulication_capo_order) - 1))
+        sampled_index = np.array([(len(self.dupulication_capo_order) - 1) * i for i in range(len(X_dup_neg) // (len(self.dupulication_capo_order) - 1))]) + diff
+        X_dup_neg_sampled = X_dup_neg[sampled_index]
+        assert len(X_dup_neg_sampled) == len(X_dup_pos)
+        X_sampled = np.concatenate([X_dup_pos, X_dup_neg_sampled])
+        y_sampled = np.concatenate([np.ones(len(X_dup_pos)), -np.ones(len(X_dup_neg_sampled))])
+        assert len(X_sampled) == len(y_sampled)
+        return self.shuffle_samples(X_sampled, y_sampled)
+
+    def create_labels(self, y):
+        """おすすめcapo(=y)を用いてlabelsを作成.(負例はこの段階ではその他全てで作る.この後negative samplingして正例と揃える)"""
+        labels = []
+        for rec in y:
+            pos_j = int(np.where(self.dupulication_capo_order == rec)[0])
+            for j in range(len(self.dupulication_capo_order)):
+                if j != pos_j:
+                    labels.append(-1)
+                else:
+                    labels.append(1)
+        return np.array(labels)
+
+    def capo_onehot_encode(self, duplicated_capos):
+        """文字列のcapoをonehot encoding"""
+        if self.capo_encoder is None:
+            self.capo_encoder = LabelBinarizer()
+            return self.capo_encoder.fit_transform(duplicated_capos)
+        else:
+            return self.capo_encoder.transform(duplicated_capos)
+
+    def duplicate_capos(self, n):
+        """n回dupulication_capo_orderを繰り返す."""
+        return np.tile(self.dupulication_capo_order, n)
+
+    def duplicate_add_capo_features(self, X):
+        """
+        :param X: np.arrayでchord featureのみ。複製前.
+        """
+        expanded_chord_features = X.repeat(len(self.dupulication_capo_order), axis=0)
+        duplicated_capos = self.duplicate_capos(len(X))
+        capo_features = self.capo_onehot_encode(duplicated_capos)
+        return np.concatenate([expanded_chord_features, capo_features], 1)
