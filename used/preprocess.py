f9a3f6c32e0c004803c5b10e8c575bb278580e37
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2019-12-06 21:55:43 +0900

    前処理修正

--- used/preprocess.py
+++ used/preprocess.py
@@ -13,7 +13,20 @@ pre_dict = {
     "N.C": "",
     "N.D.": "",
     "N.D": "",
-    "N.C,": ""
+    "N.C,": "",
+    "Im": "",
+    "N,C.": "",
+    ")": "",
+    "(": "",
+    "Ed": "",
+    "SUS": "sus",
+    "Sus": "sus",
+    "D7何も見えGm": "",
+    "Add": "add",
+    "Bm/A/愛E": "",
+    "0F": "",
+    "/A": "",
+    '/F#': ""
 }
 
 
@@ -25,11 +38,28 @@ def multiple_replace(text, adict):
     return rx.sub(one_xlat, text)
 
 
+def remove_empty_string(chord_list):
+    result = []
+    for chord in chord_list:
+        if len(chord) != 0:
+            result.append(chord)
+    return result
+
+
+capo_dict = {"0": "capo0", "+1": "harf_down", "-1": "capo1", "-2": "capo2",
+             "-3": "capo3", "-4": "capo4", "-5": "capo5", "-6": "capo6", "-7": "capo7",
+             "+2": "whole_down"}
+
+
 if __name__ == "__main__":
     path = "data/raw_data_50k.txt"
     with open(path) as f:
         for L in f:
             L = L.rstrip()
             L = L.split(" ")
+            if L[-1] not in capo_dict.keys():
+                continue
+            capo = capo_dict[L[-1]]
             modified = multiple_replace(" ".join(L[:-1]), pre_dict)  # 表記ゆれを除去
-            print(modified, L[-1])
+            removed = remove_empty_string(modified.split(" "))
+            print(" ".join(removed), capo)
